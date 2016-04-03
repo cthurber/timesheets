edbfca6d11cf4aacdac4f1dd71a9835580345e6b
dgiraldo313 <dgiraldo313@drew.edu>
2016-04-03 11:25:20 -0400

    log in box finished

---
+++ app/assets/javascripts/login.js
@@ -0,0 +1,16 @@
+var addButton= document.getElementsByClassName("add")[0];
+var container= document.getElementsByClassName("container")[0];
+var close= document.getElementsByClassName("close")[0];
+
+// will add active class to container to activate css properties
+console.log(document.getElementsByClassName("container"));
+console.log(addButton);
+console.log(close);
+addButton.addEventListener("click", function(){
+    container.className += " " + "active";
+});
+
+// will remove the active class
+close.addEventListener("click", function(){
+    container.className = "container";
+});
